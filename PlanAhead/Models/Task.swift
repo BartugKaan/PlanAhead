ab7b5b1e7df9a4ff9c370a612167a549cb7b9d5f
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-11-11 14:34:23 +0300

    add: Models

---
+++ PlanAhead/Models/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  PlanAhead
+//
+//  Created by Bartuğ Kaan Çelebi on 11.11.2024.
+//
+
+import Foundation
---
+++ PlanAhead/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  PlanAhead
+//
+//  Created by Bartuğ Kaan Çelebi on 11.11.2024.
+//
+
+import Foundation
