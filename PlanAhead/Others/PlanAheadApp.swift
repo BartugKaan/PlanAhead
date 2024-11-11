30aab00d432b0647209a6719df79bb1f83ec7c5e
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-11-11 14:37:41 +0300

    add: Firebase Configuration

--- PlanAhead/Others/PlanAheadApp.swift
+++ PlanAhead/Others/PlanAheadApp.swift
@@ -4,11 +4,16 @@
 //
 //  Created by Bartuğ Kaan Çelebi on 11.11.2024.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct PlanAheadApp: App {
+    
+    init () {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainView()
