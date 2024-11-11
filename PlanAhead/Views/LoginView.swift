07ebec72efa40d4f1ba8da5818bc736260e9ee73
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-11-11 15:27:11 +0300

    update: Login View UI design

--- PlanAhead/Views/LoginView.swift
+++ PlanAhead/Views/LoginView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                // Header
+                HeaderView()
+                // Input Field - Email, Password, Button
+                Form{
+                    TextField("Email", text: $email)
+                    SecureField("Password", text: $password)
+                    Button {
+                        
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 15)
+                                .foregroundStyle(Color("SecondaryColor"))
+                            Text("Login")
+                                .foregroundStyle(Color("LogoTextColor"))
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                                .padding(.all, 8)
+                        }
+                    }
+                    .padding(8)
+                }
+                .scrollDisabled(true)
+                .frame(width: UIScreen.main.bounds.width, height: 250)
+                Spacer()
+                // Footer
+                VStack{
+                    Text("Are you new in here ?")
+                    NavigationLink("Create new Account") {
+                        RegisterView()
+                    }
+                    .foregroundStyle(Color("SecondaryColor"))
+                }
+            }
+        }
     }
 }
 
